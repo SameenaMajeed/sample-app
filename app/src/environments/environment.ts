af4a2911a66d8e8b4f8945bc6c52487a573795c2
Sameena <sameena.majeed@neutrinos.com>
2026-04-24 15:03:14 +0530

    Initial commit

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "neutrinos-training",
+        "appName": "sample-app",
+        "namespace": "com.neutrinos-training.sample-app",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
